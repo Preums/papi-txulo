258802dcd5d3c10c24bd15a30c481368d9f32334
Preums <preums@gmx.com>
2018-03-13 10:32:47 +0100

    add of the smooth scrollspy

--- js/controler.js
+++ js/controler.js
@@ -1,3 +1,26 @@
+jQuery(document).ready(function($) {$(document).ready(function () {
+		// Active navbar status
+		$('nav ul li a').click(function (e) {
+	        e.preventDefault();
+	        $('nav ul li a').removeClass('active');
+	        $('nav ul li a').css('text-decoration','none');
+	        $(this).addClass('active');
+	        $(this).css('text-decoration','underline');              
+		});            
+		 	// Scrollspy fluide
+		$(function() {
+			$('header a').on('click', function(e) {
+				e.preventDefault();
+				var hash = this.hash;
+				$('html, body').animate({
+					scrollTop: $(this.hash).offset().top
+				}, 1000, function() {
+					window.location.hash = hash;
+				});
+			});
+		});       			            
+	});
+});
 
         function menuTable(file) {
         	$.getJSON("js/data/" +file+".json", function( data ) {
